8fe8b1bc8956e061a3e3798b0518478229997ffe
Austie <wustinah20@gmail.com>
2022-12-10 20:36:56 -0800

    Removed done comments and added some more functionality to storing plans in local storage. Also jquerified my code to show api use

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,30 @@
+//I am hoping no one has a keyboard shorcut for this character
+let seperator = "\u0482";
+let todaysPlans = [];
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  let timeBlocks = $(".time-block");
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  for(let bodyElem of timeBlocks){
+    bodyElem.click();
+  }
+
+
   // Done: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   let currentHour = dayjs().hour();
-  for(let bodyElem of document.body.querySelectorAll(".time-block")){
+  for(let bodyElem of timeBlocks){
     let hour = bodyElem.id.slice(bodyElem.id.indexOf("-") + 1);
     if(hour == currentHour){
       bodyElem.classList.add("present");
@@ -26,11 +35,25 @@ $(function () {
     }
   }
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
+  // Done: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  
 
+  //Will use an array to store the value with the current hour
+  //it will be formatted as ["hourid(seperator)text"] with (seperator) = \u0482
+  //for example: localPlans[0] could return "hour-4҂go to doctors office"
+  let localPlans = JSON.parse(localStorage.getItem("localPlans"));
+  if(localPlans !== null){
+    todaysPlans = localPlans;
+    updatePlans();
+  }
+
+  let updatePlans = function(){
+    for(let plan of todaysPlans){
+      let textSplit = plan.textSplit(seperator);
+      $("#" + textSplit[0]).children("textarea").text(textSplit[1]);
+    }
+  }
 
 
   // Done: Add code to display the current date in the header of the page.
@@ -49,6 +72,6 @@ $(function () {
       currentDate += "th";
       break;
   }
-  document.querySelector("#currentDay").textContent = currentDate;
+  $("#currentDay").text(currentDate);
 
 });
