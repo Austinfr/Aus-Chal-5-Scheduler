31e1ae11bd6620f4bd2995066ee6af80422a30b2
Austie <wustinah20@gmail.com>
2022-12-10 21:34:50 -0800

    Removed given comments and added my own to describe functionality and changed js to be more consistent

--- assets/script.js
+++ assets/script.js
@@ -6,26 +6,22 @@ let todaysPlans = [];
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  //function for storing the values 
   function storePlan(time, plan){
+    //adds the stored values to the array and then pushes it to storage
+    //it will be formatted as "hourid(seperator)text" with (seperator) = \u0482
+    //for example it could be stored as: "hour-4҂go to doctors office"
     todaysPlans.push(time + "" + seperator + "" + plan);
     localStorage.setItem("localPlans", JSON.stringify(todaysPlans));
   }
 
-  // Done: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  //gets the current hour in 24-hour time using Day.js
   let currentHour = dayjs().hour();
+  //sets the class and adds a click listener at the same time
   $(".time-block").each(function(){
     //this chunk sets the classes for where its at
     let hour = $(this).attr("id").slice($(this).attr("id").indexOf("-") + 1);
+    //I wanted to use switch case but this will work
     if(hour == currentHour){
       $(this).addClass("present");
     }else if(hour > currentHour){
@@ -37,36 +33,36 @@ $(function () {
 
     //this chunk adds a click event listener for each save button
     $(this).click(function(event){
+      //if the save button part is clicked it will store what the user has been input
       if($(event.target).hasClass("saveBtn")){
         storePlan($(this).attr("id"), $(this).children("textarea").val());
       }
     });
   });
 
-  // Done: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
   //Will use an array to store the value with the current hour
   //it will be formatted as ["hourid(seperator)text"] with (seperator) = \u0482
   //for example: localPlans[0] could return "hour-4҂go to doctors office"
   let updatePlans = function(){
+    //for each stored plan it writes it to the appropriate section
     for(let plan of todaysPlans){
+      //splits the element because of how I have it stored
       let textSplit = plan.split(seperator);
-      $("#" + textSplit[0]).children("textarea").text(textSplit[1]);
+      //gets the correct element and applies whats saved for it
+      $("#" + textSplit[0]).children("textarea").val(textSplit[1]);
     }
   }
 
+  //checks if there's any plans the user has already saved
   let localPlans = JSON.parse(localStorage.getItem("localPlans"));
   if(localPlans !== null){
     todaysPlans = localPlans;
     updatePlans();
   }
 
-
-
-  // Done: Add code to display the current date in the header of the page.
+  //formats the date to how it's wanted and displays at the top of the page
   let currentDate = dayjs().format("dddd, MMMM D");
+  //depending on what day it is it will add the appropriate suffix
   switch(currentDate.slice(currentDate.length - 2).trim()){
     case '1':
       currentDate += "st";
@@ -81,6 +77,7 @@ $(function () {
       currentDate += "th";
       break;
   }
+  //sets the text at the top
   $("#currentDay").text(currentDate);
 
 });
